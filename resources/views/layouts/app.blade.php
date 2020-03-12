c024fb431a7a5248c77835eb4a0355a225ff86ab
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-03-12 21:21:20 +0530

    Configs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,19 @@
     <title>{{ config('app.name', 'COEP') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="{{ asset('/static/js/app.js') }}" defer></script>
     
     
-    <script src="{{ asset('/js/bootstrap-datepicker.min.js') }}" defer></script>
+    <script src="{{ asset('/static/js/bootstrap-datepicker.min.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/static/css/app.css') }}" rel="stylesheet">
     
-    <link href="{{ asset('/css/bootstrap-datepicker3.css') }}" rel="stylesheet">
+    <link href="{{ asset('/static/css/bootstrap-datepicker3.css') }}" rel="stylesheet">
     
 </head>
 <body>
