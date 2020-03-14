95f45396f7199e1a1fc14c971241effee2560ba7
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-03-14 22:01:05 +0530

    Migration updates. OMG

--- database/migrations/2019_10_14_153722_register_user.php
+++ database/migrations/2019_10_14_153722_register_user.php
@@ -20,7 +20,8 @@ class RegisterUser extends Migration {
             $table->string('surName');
             $table->enum('category', ['OPEN', 'OBC', 'EWS', 'SC', 'ST', 'SBC', 'VJ', 'NT-1', 'NT-2', 'NT-3', 'ECBC', 'OTHER']);
             $table->enum('gender', ['male', 'female', 'other']);
-            $table->date('yearOfAdmission');
+            //$table->date('yearOfAdmission');
+            $table->year('yearOfAdmission');
             $table->string('contact');
             $table->string('college');
             
---
+++ database/migrations/2020_03_14_152640_create_ssc_hsc_diploma_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSscHscDiplomaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ssc_hsc_diploma', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('id')->references('id')->on('registerusers')->onDelete('cascade');
+            
+            $table->float('ssc')->nullable();
+            $table->year('sscYear')->nullable();
+             
+            $table->float('hcs')->nullable();
+            $table->year('hcsYear')->nullable();
+            
+            $table->float('diploma')->nullable();
+            $table->year('diplomaYear')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ssc_hsc_diploma');
+    }
+}
