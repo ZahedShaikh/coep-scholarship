34c1c196217b2f3678119720e708003223f6b36e
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-04-05 23:32:11 +0530

    Workaround: Admin-Account-section done

--- app/Http/Controllers/accountantController.php
+++ app/Http/Controllers/accountantController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class accountantController extends Controller {
 
     public function index() {
-        return view('admin.auth.Amountant');
+        return view('admin.accountant.amountDistribute');
     }
 
     public function show(Request $request) {
@@ -20,23 +20,24 @@ class accountantController extends Controller {
             $query = $request->get('query');
 
             if ($query != '') {
-
-                $data = DB::table('registerusers')->join('scholarship_status', function ($join) {
-                            $join->on('registerusers.id', '=', 'scholarship_status.id');
-                        })
+                $data = DB::table('registerusers')
+                        ->join('amount_sanctioned_by_issuer', 'registerusers.id', '=', 'amount_sanctioned_by_issuer.id')
+                        ->join('bank_details', 'registerusers.id', '=', 'bank_details.id')
                         ->where('registerusers.id', 'LIKE', '%' . $query . '%')
                         ->orWhere('registerusers.name', 'LIKE', '%' . $query . '%')
-                        ->where('scholarship_status.in_process_with', '=', 'accountant')
-                        ->orderBy('registerusers.id', 'desc')
+//                        ->select('registerusers.name', 'registerusers.middleName', 'registerusers.surName', 'registerusers.contact', 'registerusers.college',
+//                                'bank_details.bank_Name', 'bank_details.IFSC_Code', 'bank_details.account_No', 'bank_details.branch',
+//                                'amount_sanctioned_by_issuer.amount', 'amount_sanctioned_by_issuer.now_receiving_amount_for_semester')
+                        ->orderBy('registerusers.college', 'ASC')
                         ->get();
             } else {
-
                 $data = DB::table('registerusers')
-                        ->join('scholarship_status', function ($join) {
-                            $join->on('registerusers.id', '=', 'scholarship_status.id');
-                        })
-                        ->where('scholarship_status.in_process_with', '=', 'accountant')
-                        ->orderBy('registerusers.id', 'desc')
+                        ->join('amount_sanctioned_by_issuer', 'registerusers.id', '=', 'amount_sanctioned_by_issuer.id')
+                        ->join('bank_details', 'registerusers.id', '=', 'bank_details.id')
+//                        ->select('registerusers.name', 'registerusers.middleName', 'registerusers.surName', 'registerusers.contact', 'registerusers.college',
+//                                'bank_details.bank_Name', 'bank_details.IFSC_Code', 'bank_details.account_No', 'bank_details.branch',
+//                                'amount_sanctioned_by_issuer.amount', 'amount_sanctioned_by_issuer.receiving_amount_for_semester')
+                        ->orderBy('registerusers.college', 'ASC')
                         ->get();
             }
 
@@ -44,25 +45,23 @@ class accountantController extends Controller {
 
             if ($total_row > 0) {
                 foreach ($data as $row) {
-
                     $fullName = $row->name . " " . $row->middleName . " " . $row->surName;
-                    $amount = ($row->now_receiving_amount_for_semester - $row->prev_amount_received_in_semester) * 4000;
-
                     $output .= '
                     <tr id=\"' . $row->id . '\">
                     <td align=\'center\'>' . $row->id . '</td>
                     <td>' . $fullName . '</td>
                     <td>' . $row->college . '</td>
                     <td>' . $row->contact . '</td>
-                    <td>' . $amount . "</td>
-                    <td> <a onclick=\"$(this).assign('$row->id, $amount')\" class=\"btn btn-primary align-content-md-center\">Sanction Amount</a> </td>
+                    <td>' . $row->receiving_amount_for_semester . '</td>
+                    <td>' . $row->amount . "</td>
+                    <td> <a onclick=\"$(this).assign('$row->id')\" class=\"btn btn-primary align-content-md-center\">Sanction Amount</a> </td>
                     </tr>
                     ";
                 }
             } else {
                 $output = '
             <tr>
-            <td align="center" colspan="6">No Data Found</td>
+            <td align="center" colspan="7">No Data Found</td>
             </tr>
             ';
             }
@@ -94,68 +93,38 @@ class accountantController extends Controller {
     public function send(Request $request) {
 
         if ($request->ajax()) {
-            $output = false;
-
-            $str_arr = explode(",", trim($request->get('query')));
-            $studentID = $str_arr[0];
-            $amount = (float) $str_arr[1];
-
+            $studentID = $request->get('query');
             try {
                 DB::beginTransaction();
 
-                $temp = DB::table('scholarship_status')
-                        ->where('scholarship_status.id', '=', $studentID)
-                        ->join('registerusers', 'scholarship_status.id', '=', 'registerusers.id')
-                        ->select('scholarship_status.now_receiving_amount_for_semester', 'registerusers.yearOfAdmission')
+                $temp = DB::table('amount_sanctioned_by_issuer')
+                        ->where('id', '=', $studentID)
                         ->first();
 
                 DB::table('scholarship_status')
                         ->where('id', $studentID)
-                        ->update(['prev_amount_received_in_semester' => $temp->now_receiving_amount_for_semester,
-                            'in_process_with' => 'issuer']);
-
-                $months = date('m');
-                $addMonths = 0;
-                switch ($months) {
-                    case ($months >= 7 and $months <= 11):
-                        // This case for Semeseter 1
-                        $addMonths = 2;
-                        break;
-                    case ($months >= 1 and $months <= 5):
-                        // This case for Semeseter 2
-                        $addMonths = 1;
-                        break;
-                    default:
-                        // This case holidays
-                        $addMonths = 0;
-                }
-
-                // Substracting 1 since I don't wanna count current year
-                // Instead I will count $addMonths
-                $years = date("Y") - date('Y', strtotime($temp->yearOfAdmission)) - 1;
-                $forSemester = 1 + $addMonths + $years * 2;
-                $years = $years + 1;
+                        ->update(['in_process_with' => 'issuer', 
+                            'prev_amount_received_in_semester'=> $temp->receiving_amount_for_semester]);
 
                 DB::table('transaction_history')->insert(
                         ['id' => $studentID,
                             'dateOfTransaction' => now(),
-                            'amount' => intval($amount),
-                            'amountReceivedForYear' => $years,
-                            'amountReceivedForSemester' => $forSemester,
+                            'amount' => $temp->amount,
+                            'amountReceivedForYear' => ceil($temp->receiving_amount_for_semester / 2),
+                            'amountReceivedForSemester' => $temp->receiving_amount_for_semester,
                             'year' => date("Y"),
                             'created_at' => Carbon::now(),
                             'updated_at' => now()]
                 );
-
+                
                 DB::table('amount_sanctioned_by_issuer')->where('id', '=', $studentID)->delete();
                 DB::commit();
-                $output = true;
             } catch (\Exception $e) {
                 DB::rollback();
-                return redirect(route('admin.auth.getAmountToBeCredit'))->with('message', 'Something went wrong');
+                dd('Something went wrong - admin.auth.accountcontroller@send', $e);
             }
 
-            echo json_encode($output);
+            echo json_encode(true);
         }
     }
 
--- resources/views/admin/accountant/home.blade.php
+++ resources/views/admin/accountant/home.blade.php
@@ -1,14 +1,15 @@
-@extends('multiauth::layouts.app') 
+@extends('admin.layout.app')
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ ucfirst(config('multiauth.prefix')) }} Dashboard</div>
+                <div class="card-header">{{ ucfirst(config('auth.prefix')) }} Dashboard</div>
 
                 <div class="card-body">
-                    You are logged in to {{ config('multiauth.prefix') }} side!
-                    @include('multiauth::message')
+                    You are logged in to {{ config('auth.prefix') }} side!
+                    @include('message')
                 </div>
 
                 <ol class="list-group">
@@ -19,16 +20,12 @@
                         </li>
                     </div>
                     
-                    
                     <div class="float-right">
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             <label for="displayAll" class="col-md-6 col-form-label text-md-right">{{ __('Display All') }}</label>
-                            <a href="{{ route('getNewApplications') }}" class="btn btn-group-toggle btn-primary mr-3">Show</a>
+                            <a href="{{ route('getAmountToBeCredit') }}" class="btn btn-group-toggle btn-primary mr-3">Show</a>
                         </li>
                     </div>
-
-
-
                 </ol>
 
             </div>
