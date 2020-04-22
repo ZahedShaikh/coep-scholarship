cee4319081848fc1d5ff233d094e4cd0b1c26dd1
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-04-23 00:33:27 +0530

    Workaround: Vendor UI

--- resources/views/vendor/auth/login.blade.php
+++ resources/views/vendor/auth/login.blade.php
@@ -1,12 +1,13 @@
-@extends('vendor.layout.auth')
+@extends('vendor.layout.app')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ ucfirst(config('Auth.prefix')) }} Login</div>
+
+                <div class="card-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/vendor/login') }}">
                         {{ csrf_field() }}
 
--- resources/views/vendor/auth/register.blade.php
+++ resources/views/vendor/auth/register.blade.php
@@ -1,16 +1,17 @@
-@extends('vendor.layout.auth')
+@extends('vendor.layout.app')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ ucfirst(config('Auth.prefix')) }} Register</div>
+
+                <div class="card-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/vendor/register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} row">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
