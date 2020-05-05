efcdf90317aaab1bb47d0d9746e9e3379a093a47
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-05-05 23:38:26 +0530

    Feature: Colleges VS Students chart

--- app/Http/Controllers/Charts.php
+++ app/Http/Controllers/Charts.php
@@ -2,34 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use ArielMejiaDev\LarapexCharts\LarapexChart;
+use Illuminate\Support\Facades\DB;
 
 class Charts extends Controller {
 
     public function index() {
-        $chart = (new LarapexChart)->setTitle('Net Profit')
-                ->setSubtitle('From January To March')
-                ->setType('bar')
-                ->setXAxis(['Jan', 'Feb', 'Mar'])
-                ->setGrid(true)
-                ->setDataset([
-                    [
-                        'name' => 'Company A',
-                        'data' => [500, 1000, 1900]
-                    ],
-                    [
-                        'name' => 'Company B',
-                        'data' => [300, 900, 1400]
-                    ],
-                    [
-                        'name' => 'Company C',
-                        'data' => [430, 245, 500]
-                    ]
-                ])
-                ->setStroke(1);
+        
+        $coep = [];
+        $gcoer = [];
+        $gcoek = [];
+        $gpp = [];
+        $gpa = [];
 
-        return view('vendor.multiauth.charts-and-details.collageVsYear', compact('chart'));
+        for ($i = 1; $i <= 4; $i++) {
+            $coep[$i] = DB::table('transaction_history')
+                    ->join('registerusers', 'transaction_history.id', '=', 'registerusers.id')
+                    ->where('transaction_history.amountReceivedForYear', '=', $i)
+                    ->where('registerusers.college', '=', 'coep')
+                    ->count();
+        }
+        for ($i = 1; $i <= 4; $i++) {
+            $gcoer[$i] = DB::table('transaction_history')
+                    ->join('registerusers', 'transaction_history.id', '=', 'registerusers.id')
+                    ->where('transaction_history.amountReceivedForYear', '=', $i)
+                    ->where('registerusers.college', '=', 'gcoer')
+                    ->count();
+        }
+        for ($i = 1; $i <= 4; $i++) {
+            $gcoek[$i] = DB::table('transaction_history')
+                    ->join('registerusers', 'transaction_history.id', '=', 'registerusers.id')
+                    ->where('transaction_history.amountReceivedForYear', '=', $i)
+                    ->where('registerusers.college', '=', 'gcoek')
+                    ->count();
+        }
+        for ($i = 1; $i <= 3; $i++) {
+            $gpp[$i] = DB::table('transaction_history')
+                    ->join('registerusers', 'transaction_history.id', '=', 'registerusers.id')
+                    ->where('transaction_history.amountReceivedForYear', '=', $i)
+                    ->where('registerusers.college', '=', 'gpp')
+                    ->count();
+        }
+        for ($i = 1; $i <= 3; $i++) {
+            $gpa[$i] = DB::table('transaction_history')
+                    ->join('registerusers', 'transaction_history.id', '=', 'registerusers.id')
+                    ->where('transaction_history.amountReceivedForYear', '=', $i)
+                    ->where('registerusers.college', '=', 'gpa')
+                    ->count();
+        }
+        //dd($coep, $gcoer, $gcoek, $gpp, $gpa);
+
+        $college = array('COEP', 'GCOER', 'GCOEK', 'GPP', 'GPA');
+        $FY = array($coep[1], $gcoer[1], $gcoek[1], $gpp[1], $gpa[1]);
+        $SY = array($coep[2], $gcoer[2], $gcoek[2], $gpp[2], $gpa[2]);
+        $TY = array($coep[3], $gcoer[3], $gcoek[3], $gpp[3], $gpa[3]);
+        $BE = array($coep[4], $gcoer[4], $gcoek[4]);
+        return view('charts-and-details.charts', ['College' => $college,
+            'Data1' => $FY, 'Data2' => $SY, 'Data3' => $TY, 'Data4' => $BE]);
     }
-
+    
 }
