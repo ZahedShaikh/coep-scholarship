bae7914192fecf217b4860622ff4c7e662c49ba3
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-05-09 12:08:43 +0530

    Fix - Migrated from Bootstrap-min to Bootstrap-bundle-min

--- resources/views/accountant/layout/appToSearch.blade.php
+++ resources/views/accountant/layout/appToSearch.blade.php
@@ -9,10 +9,10 @@
         <!-- CSRF Token -->
         <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }} {{ ucfirst(config('auth.prefix')) }}</title>
+        <title>{{ config('app.name', 'Laravel') }} {{ ucfirst(config('multiauth.prefix')) }}</title>
 
         <!-- Scripts -->
-        <script src="{{ asset('/js/app.js') }}" defer></script>
+<!--        <script src="{{ asset('/js/app.js') }}" defer></script>-->
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -23,8 +23,11 @@
 
         <!-- Ajax and Bootstrap CDN -->
         <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+        <!--
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
+        -->
     </head>
 
     <div id="app">
@@ -52,7 +55,7 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="/admin/logout" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
+                                        document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
                                 <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
--- resources/views/admin_common_layouts/appToSearch.blade.php
+++ resources/views/admin_common_layouts/appToSearch.blade.php
@@ -12,7 +12,7 @@
         <title>{{ config('app.name', 'Laravel') }} {{ ucfirst(config('multiauth.prefix')) }}</title>
 
         <!-- Scripts -->
-        <script src="{{ asset('/js/app.js') }}" defer></script>
+<!--        <script src="{{ asset('/js/app.js') }}" defer></script>-->
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -21,66 +21,68 @@
         <!-- Styles -->
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
+        <!-- Ajax and Bootstrap CDN -->
+        <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+        <!--
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        -->
     </head>
 
     <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/accountant/home') }}">
-                        TATA Samarth Scholarship
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/accountant/home') }}">
+                    TATA Samarth Scholarship
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-                        </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest('accountant')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/accountant/home') }}">{{ ucfirst(config('auth.prefix')) }} Login</a>
-                            </li>
-                            @else
-                            <li class="nav-item dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ auth('accountant')->user()->name }} <span class="caret"></span>
-                                </a>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest('accountant')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/accountant/home') }}">{{ ucfirst(config('auth.prefix')) }} Login</a>
+                        </li>
+                        @else
+                        <li class="nav-item dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ auth('accountant')->user()->name }} <span class="caret"></span>
+                            </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/accountant/logout') }}"
-                                           onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ url('/accountant/logout') }}"
+                                       onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
 
-                                        <form id="logout-form" action="{{ url('/accountant/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                            @endguest
-                        </ul>
-                    </div>
+                                    <form id="logout-form" action="{{ url('/accountant/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                        @endguest
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
 
-            <main class="py-4">
-                @yield('content')
-            </main>
-        </div>
-    </body>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
 
 </html>
